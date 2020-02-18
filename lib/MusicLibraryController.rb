d43c7d21159d2bae9ddb3e0eb98604b808833363
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-18 13:56:17 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -43,6 +43,14 @@ class MusicLibraryController
     end
   end
   
+  def list_artists
+    Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |a, i|
+      puts "#{i+1}. #{a.name}"
+    end
+  end
+  
+  
+  
        
      
    end
